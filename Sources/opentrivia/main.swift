484944d47a08b88612b960774c007ccdc21d75ee
Periicles <landescrew.scoot@gmail.com>
2024-12-03 21:38:25 +0100

    feat: add the correct elements for main

--- Sources/opentrivia/main.swift
+++ Sources/opentrivia/main.swift
@@ -2,15 +2,18 @@
 // https://docs.swift.org/swift-book
 
 import Foundation
-import Dispatch
 
-print("Open Trivia")
+// MARK: - Dispatch
 
 // NB: Replace the `DispatchQueue.main.asyncAfter` with your code.
 // Once the program is done, call `exit(EXIT_SUCCESS)` to exit the program.
-DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-    print("I'm done!")
-    exit(EXIT_SUCCESS)
+let api = OpenTriviaAPI()
+let repository = OpenTriviaRepository(api: api)
+let useCase = GetTriviaUseCase(repository: repository)
+let cli = TriviaCLI(getTriviaUseCase: useCase)
+
+DispatchQueue.main.async {
+    cli.start()
 }
 
 dispatchMain()
